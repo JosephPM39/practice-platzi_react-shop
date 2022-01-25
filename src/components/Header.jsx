b686a41b2f9906b364d9e6e8d432d7fd1eafe316
JosephPM39 <josias.jjpm49@hotmail.com>
2022-01-24 21:12:55 -0600

    Agregando demas componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
-import "../styles/Header.scss";
+import React, { useState } from 'react';
+import "@styles/Header.scss";
+import menu from '@icons/icon_menu.svg';
+import MenuComponent from '@components/Menu';
+import logo from '@logos/logo_yard_sale.svg';
+import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
     return (
         <nav>
-        <img src="./icons/icon_menu.svg" alt="menu" className="menu"/>
-        <div className="navbar-left">
-            <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
-            <ul>
-                <li>
-                    <a href="/">Alls</a>
-                </li>
-                <li>
-                    <a href="/">Clhotes</a>
-                </li>
-                <li>
-                    <a href="/">Electronics</a>
-                </li>
-                <li>
-                    <a href="/">Furtinures</a>
-                </li>
-                <li>
-                    <a href="/">Toys</a>
-                </li>
-                <li>
-                    <a href="/">Others</a>
-                </li>
-            </ul>
-        </div>
-        <div className="navbar-right">
-            <ul>
-                <li className="navbar-email">platzi@example.com</li>
-                <li className="navbar-shopping-cart">
-                    <img src="./icons/icon_shopping_cart.svg" alt="icon_shopping_cart"/>
-                    <div>2</div>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <img src={menu} alt="menu" className="menu" />
+            <div className="navbar-left">
+                <img src={logo} alt="logo" className="nav-logo " />
+                <ul>
+                    <li>
+                        <a href="/">Alls</a>
+                    </li>
+                    <li>
+                        <a href="/">Clhotes</a>
+                    </li>
+                    <li>
+                        <a href="/">Electronics</a>
+                    </li>
+                    <li>
+                        <a href="/">Furtinures</a>
+                    </li>
+                    <li>
+                        <a href="/">Toys</a>
+                    </li>
+                    <li>
+                        <a href="/">Others</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <ul>
+                    <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
+                    <li className="navbar-shopping-cart">
+                        <img src={shoppingCart} alt="icon_shopping_cart" />
+                        <div>2</div>
+                    </li>
+                </ul>
+            </div>
+            {toggle && <MenuComponent />}
+        </nav>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '@components/Header';
+import ProductList from '@containers/ProductList';
 
 const Home = () => {
     return (
         <div>
-            <Header/>
-            <h1>
-                React Shop
-            </h1>
+            <Header />
+            <>
+                <ProductList />
+            </>
         </div>
     );
 }
