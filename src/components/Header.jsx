43fca5576fc0994371472d4026db5ed87c9d5b9a
JosephPM39 <josias.jjpm49@hotmail.com>
2022-02-27 17:14:58 -0600

    Agregando funcionalidades a ordenes y carrito de compras

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import "@styles/Header.scss";
 import menu from '@icons/icon_menu.svg';
 import MenuComponent from '@components/Menu';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from "@icons/icon_shopping_cart.svg";
+import AppContext from '@context/AppContext'
+import MyOrder from '@containers/MyOrder';
 
 const Header = () => {
 
@@ -11,6 +13,10 @@ const Header = () => {
     const handleToggle = () => {
         setToggle(!toggle);
     }
+
+    const { state: { cart } } = useContext(AppContext);
+
+    const [toggleOrders, setToggleOrders] = useState(false);	
 
     return (
         <nav>
@@ -38,16 +44,17 @@ const Header = () => {
                     </li>
                 </ul>
             </div>
-            <div className="navbar-right">
+            <div className="navbar-right" >
                 <ul>
                     <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
-                    <li className="navbar-shopping-cart">
+                    <li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
                         <img src={shoppingCart} alt="icon_shopping_cart" />
-                        <div>2</div>
+                        {cart.length > 0 ? <div>{cart.length}</div> : null}
                     </li>
                 </ul>
             </div>
             {toggle && <MenuComponent />}
+            {toggleOrders && <MyOrder />}
         </nav>
     );
 }
